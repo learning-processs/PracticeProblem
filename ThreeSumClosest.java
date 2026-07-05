75695be718200ff2a0ef636a6c38aa7556659c91
Anu025 <anu001125@gmail.com>
2026-07-06 00:31:49 +0530

    ThreeSum Closest

--- ThreeSumClosest.java
+++ ThreeSumClosest.java
@@ -8,34 +8,39 @@ public class ThreeSumClosest {
 
         int closest = nums[0] + nums[1] + nums[2];
 
-        for(int i = 0 ; i < nums.length - 2 ; i++){
+        for (int i = 0; i < nums.length - 2; i++) {
 
             int left = i + 1;
             int right = nums.length - 1;
 
-            int sum = nums[i] + nums[left] + nums[right];
+            while (left < right) {
 
-            if(Math.abs(target - sum) < Math.abs(target - closest)){
-                closest = sum;
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(target - sum) < Math.abs(target - closest)) {
+                    closest = sum;
+                }
+
+                if (sum == target) {
+                    return sum;
+                } 
+                else if (sum < target) {
+                    left++;
+                } 
+                else {
+                    right--;
+                }
             }
-
-            if(sum == target){
-                return sum;
-            }
-
-            else if( sum < target){
-                left++;
-            }else{
-                right --;
-            }
-
         }
 
         return closest;
     }
-    public static void main(String[] args) {
-       int  nums [] = {-1,2,1,-4}; int target = 1;
 
-       System.out.println(threeSumClosest(nums, target));
+    public static void main(String[] args) {
+
+        int nums[] = {4, 0, 5, -5, 3, 3, 0, -4, -5};
+        int target = -2;
+
+        System.out.println(threeSumClosest(nums, target));
     }
 }
