b5acc1a46dcb3b621e36f0b961aaf5b2d02cd940
Anu025 <anu001125@gmail.com>
2026-07-06 23:49:57 +0530

    4Sum

---
+++ Foursum.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Foursum {
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+
+                    long sum = nums[i]+ nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j],nums[right], nums[left]));
+                        left++;
+                        right--;
+
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+
+                }
+
+            }
+
+        }
+
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 0, -1, 0, -2, 2 };
+        int target = 0;
+
+        System.out.println(fourSum(nums, target));
+    }
+}
