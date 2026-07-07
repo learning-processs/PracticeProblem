328fb4b2fd248029259e2cde84c9692d77e5da07
Anu025 <anu001125@gmail.com>
2026-07-07 23:27:10 +0530

    Remove Element

---
+++ RemoveElement.java
@@ -0,0 +1,32 @@
+public class RemoveElement {
+    public static int removeElement(int[] nums, int val) {
+
+        int j = 0;
+
+        for(int i = 0 ; i < nums.length ; i++){
+
+            if(nums[i] != val){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+
+    }
+
+    public static void main(String[] args) {
+
+        int nums[] = { 3, 3 ,3 ,2, 2, 3 };
+        int val = 3;
+
+        int k = removeElement(nums, val);
+
+        System.out.println(" k : " + k );
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " " );
+        }
+
+    }
+}
