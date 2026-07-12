dfb7c1d093c417b5600e45706e89bf793bfafd66
Anu025 <anu001125@gmail.com>
2026-07-13 01:43:25 +0530

    LongestPalindrome

--- LengthOfLongestSubstring.java
+++ LengthOfLongestSubstring.java
@@ -5,7 +5,7 @@ public class LengthOfLongestSubstring {
     public static int lengthOfLongestSubstring(String s) {
         HashSet<Character> set  = new HashSet<>();
         int left = 0 ; int max = 0;
-        for(int right = 0 ; right < s.length() ;right++ ){
+        for(int right = 0 ; right < s.length() ; right++ ){
             while (set.contains(s.charAt(right))) {
                 set.remove(s.charAt(left));
                 left++;
@@ -13,10 +13,9 @@ public class LengthOfLongestSubstring {
             set.add(s.charAt(right));
             max = Math.max(max, right - left + 1);
         }
-
         return max;
-        
     }
+    
     public static void main(String[] args) {
        String s = "abcabcbb";
 
