7d79f6e0b1dfee8f1c6149346160a0c948b22c55
Anu025 <anu001125@gmail.com>
2026-07-14 23:24:14 +0530

    convert

---
+++ ZigzagConversion.java
@@ -0,0 +1,49 @@
+public class ZigzagConversion {
+
+    public static String convert(String s, int numRows) {
+
+        if( numRows == 1 || s.length() <= numRows){
+            return s;
+        }
+
+        StringBuilder [] rows = new StringBuilder[numRows];
+
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int currentRow = 0;
+        boolean goingDown = true;
+
+        for(char c : s.toCharArray()){
+
+            rows[currentRow].append(c);
+
+            if(currentRow == 0){
+                goingDown = true;
+            }else if(currentRow == numRows - 1){
+                goingDown = false;
+            }
+
+            if(goingDown){
+                currentRow++;
+            }
+            else
+                currentRow--;
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for( StringBuilder row : rows){
+            ans.append(row);
+        }
+
+        return ans.toString();
+    }
+    public static void main(String[] args) {
+        String s = "PAYPALISHIRING"; 
+        int numRows = 3;
+
+        System.out.println(convert(s, numRows));
+    }
+}
