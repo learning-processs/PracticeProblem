2b2da78c4e1d7b67943d275a5d737ecd86908cd8
Anu025 <anu001125@gmail.com>
2026-07-27 02:47:14 +0530

    Find the number of zeroes

---
+++ CountZero.java
@@ -0,0 +1,25 @@
+public class CountZero {
+
+    static int countZero(int arr[], int n){
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] == 0){
+                return i;
+            }
+        }
+
+        return - 1;
+    }
+    public static void main(String[] args) {
+       int arr[] = {1, 1, 1, 1, 0, 0};
+
+       int n = arr.length;
+        int x = countZero(arr, n);
+        if (x == -1) {
+            System.out.println("Count of zero is 0");
+        }
+        else {
+            System.out.print("count of zero is ");
+              System.out.println(n-x);
+        }
+    }
+}
