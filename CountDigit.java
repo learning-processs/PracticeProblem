be962cd2a848ed76ed7ff7250d7018e3c23eaa75
Anu025 <anu001125@gmail.com>
2026-08-04 03:19:33 +0530

    Count digits

---
+++ CountDigit.java
@@ -0,0 +1,24 @@
+public class CountDigit {
+
+    public static int countDigit(int n){
+        if(n == 0){
+            return 1;
+        }
+
+        int count = 0;
+
+        while (n != 0) {
+            
+            n = n/10;
+            ++count;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        
+        int n = 58964;
+        System.out.println(countDigit(n));
+    }
+}
