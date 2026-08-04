55a93bd47f7b96669dee5638d408737959e9c04b
Anu025 <anu001125@gmail.com>
2026-08-05 01:22:04 +0530

    Calculate sum of all numbers present in a string

--- NonRep.java
+++ NonRep.java
@@ -1,28 +1,29 @@
-public class NonRep {
-    public static char nonRep(String str){
+// public class NonRep {
+//     public static char nonRep(String str){
 
-        int n = str.length();
+//         int n = str.length();
 
-        for(int i = 0 ; i < n ;i++){
+//         for(int i = 0 ; i < n ;i++){
             
-            boolean found = false;
+//             boolean found = false;
 
-            for(int j = 0 ; j < n ; j++){
+//             for(int j = 0 ; j < n ; j++){
 
-                if(i != j &&  str.charAt(i) == str.charAt(j)){
-                    found = true;
-                    break;
-                }
-            }
-            if(!found){
-                return str.charAt(i);
-            }
+//                 if(i != j &&  str.charAt(i) == str.charAt(j)){
+//                     found = true;
+//                     break;
+//                 }
+//             }
+//             if(!found){
+//                 return str.charAt(i);
+//             }
            
-        }
-        return '$';
-    }
-    public static void main(String[] args) {
-        String s ="geeksforgeeks";
-        System.out.println(nonRep(s));
-    }
-}
+//         }
+//         return '$';
+//     }
+//     public static void main(String[] args) {
+//         String s ="geeksforgeeks";
+//         System.out.println(nonRep(s));
+//     }
+// }
+
