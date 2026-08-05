587c1c8985ae6627d1faee2ea1a3e8495a542cba
Anu025 <anu001125@gmail.com>
2026-08-05 23:42:27 +0530

    Calculate sum of all numbers present in a string

--- SumOfPresNum.java
+++ SumOfPresNum.java
@@ -5,21 +5,25 @@ public class SumOfPresNum {
         String temp = "0";
         int sum = 0;
 
-        for(int i = 0 ; i < str.length() ; i++){
+        for(int i = 0 ; i < str.length() ;i++){
+
             char ch = str.charAt(i);
 
             if(Character.isDigit(ch)){
                 temp +=ch;
             }else{
+
                 sum += Integer.parseInt(temp);
                 temp = "0";
             }
         }
-        return sum+ Integer.parseInt(temp);
+        return sum + Integer.parseInt(temp);
     }
-    
+
     public static void main(String[] args) {
-        String str = "12abc20yz68";
+        
+         String str = "12abc20yz68";
+
         System.out.println(findSum(str));
     }
 }
