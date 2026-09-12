b3d18268de0a3ff53534d3b9667ec38b027a30de
Anu025 <anu001125@gmail.com>
2026-09-12 11:35:53 +0530

    VisibleBuilding

---
+++ hcl/VisibleBuilding.java
@@ -0,0 +1,24 @@
+public class VisibleBuilding {
+
+    static int visibleBuildings(int arr[]){
+        int ans = 1;
+        int max = arr[0];
+
+        for(int i = 1 ; i < arr.length ; i++){
+
+            if(arr[i] >= max){
+                ans++;
+                max = arr[i];
+            }
+        }
+
+        return ans;
+    }
+
+    
+    public static void main(String[] args) {
+        int arr[] = {6, 2, 8, 4, 11, 13};
+        
+        System.out.println(visibleBuildings(arr));
+    }
+}
