87b08e1b6ea57d5f74280ca26b46aa93a0bb69f5
Anu025 <anu001125@gmail.com>
2026-09-12 16:34:28 +0530

    MiddleOfThree

---
+++ hcl/MiddleOfThree.java
@@ -0,0 +1,17 @@
+public class MiddleOfThree {
+
+    public static int middle(int a , int b , int c){
+        
+        if((a < b && b < c) || (c<b && b < a)){
+            return b;
+        }else if((b < a && a < c) || (c < a && a < b)){
+            return a;
+        }else{
+            return c;
+        }
+    }
+    public static void main(String[] args) {
+        int b = 20, a = 30, c = 40;
+        System.out.println(middle(a, b, c));
+    }
+}
