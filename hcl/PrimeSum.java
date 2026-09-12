5864899ae0720ef5fa802ab9c781bd2d00cfa563
Anu025 <anu001125@gmail.com>
2026-09-12 22:25:18 +0530

    PrimeSum

---
+++ hcl/PrimeSum.java
@@ -0,0 +1,24 @@
+public class PrimeSum {
+    public static  int primeSum(int n){
+        int sum = 0;
+
+        for(int i = 2 ; i <=n ; i++){
+            int flag = 1;
+            for(int j = 2 ; j <= i/2; j++){
+
+                if( i % j == 0){
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag == 1){
+                sum += i;
+            }
+        }
+        return  sum;
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(primeSum(n));
+    }
+}
--- hcl/PrintStatusOfDoor.java
+++ hcl/PrintStatusOfDoor.java
@@ -1,23 +1,25 @@
 public class PrintStatusOfDoor {
 
-    public static void printStatus(int n){
-        for(int i = 1 ; i <= n ; i++){
+    public static void printStatus(int n) {
+        for (int i = 1; i <= n; i++) {
             int divisor = 0;
-            for(int j = 1 ; j <= i ; j++){
-                if(i %j == 0){
-                    divisor ++;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    divisor++;
                 }
             }
 
-            if(divisor % 2 == 0){
+            if (divisor % 2 == 0) {
                 System.out.print("Closed -> ");
-            }else{
+            } else {
                 System.out.print("Open -> ");
             }
         }
     }
+
     public static void main(String[] args) {
-           int n = 5;
-           printStatus(n );
+        int n = 5;
+        printStatus(n);
     }
 }
+
