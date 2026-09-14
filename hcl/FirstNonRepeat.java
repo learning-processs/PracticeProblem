74ddcc625e9f237bb205218676bd5d42c9200531
Anu025 <anu001125@gmail.com>
2026-09-14 22:36:07 +0530

    FirstNonRepeat

---
+++ hcl/FirstNonRepeat.java
@@ -0,0 +1,23 @@
+public class FirstNonRepeat {
+    public static char nonRepeat(String s){
+
+        for(int i = 0; i < s.length() ; i++){
+            boolean found = false;
+            for(int j = 0 ; j < s.length() ; j++){
+                if(s.charAt(i) == s.charAt(j) && i != j){
+                    found = true;
+                    break;
+                }
+            }
+
+            if(!found){
+                return s.charAt(i);
+            }
+        }
+        return '0';
+    }
+    public static void main(String[] args) {
+        String s = "racecar";
+        System.out.println(nonRepeat(s));
+    }
+}
