53339fc5707043f426d885aae2ddd7752b173ef7
Anu025 <anu001125@gmail.com>
2026-09-18 22:08:29 +0530

    RemoveSpace

---
+++ hcl/RemoveSpace.java
@@ -0,0 +1,24 @@
+public class RemoveSpace {
+
+    public static String removeSpace(String str){
+        int n = str.length();
+        char[] arr = str.toCharArray();
+
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] == ' '){
+                for(int j = 0; j < n - 1 ; j++){
+                    arr[j] = arr[j + 1];
+                }
+                n--;
+                i--;
+            }
+        }
+        return new String(arr, 0 , n);
+        
+    }
+    public static void main(String[] args) {
+
+        String str = " gf g";
+        System.out.println(removeSpace(str));
+    }
+}
